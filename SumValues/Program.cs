8dfd10e409a814218227b726a6972be674996cef
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-19 16:08:57 +0100

    Add project files.

---
+++ SumValues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint oddNumberCount = 0;
+            uint evenNumberCount = 0;
+
+            int currentValue = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            while (currentValue < limit)
+            {
+
+            }
+
+
+            Console.WriteLine("Quantity of Odd Numbers: " + oddNumberCount);
+            Console.WriteLine("Quantity of Even Numbers: " + evenNumberCount);
+
+        }
+    }
+}
