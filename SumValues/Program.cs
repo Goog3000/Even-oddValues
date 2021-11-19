dd3906b4665590431ad29d3966880646fd1c5e61
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-19 17:01:33 +0100

    SuOfevenOdd

--- SumValues/Program.cs
+++ SumValues/Program.cs
@@ -9,17 +9,37 @@ namespace SumValues
             uint oddNumberCount = 0;
             uint evenNumberCount = 0;
 
+            int oddNumberSum = 0;
+            int evenNumberSum = 0;
+
+            Console.WriteLine("Enter first value of range :");
             int currentValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second value of range :");
             int limit = int.Parse(Console.ReadLine());
 
-            while (currentValue < limit)
+            while (currentValue <= limit)
             {
-
+                if (currentValue % 2 == 0)
+                {
+                    evenNumberCount++;
+                    evenNumberSum += currentValue;
+                }
+                else
+                {
+                    
+                    oddNumberCount++;
+                    oddNumberSum = oddNumberSum + currentValue;
+                }
+                currentValue++;
+                
             }
 
 
             Console.WriteLine("Quantity of Odd Numbers: " + oddNumberCount);
             Console.WriteLine("Quantity of Even Numbers: " + evenNumberCount);
+            Console.WriteLine("Sum of Odd Numbers: " + oddNumberSum);
+            Console.WriteLine("Sum of Even Numbers: " + evenNumberSum);
 
         }
     }
